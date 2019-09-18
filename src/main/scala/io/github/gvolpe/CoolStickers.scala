9adb3014b0df40b582d4073cddf579f8f4404738
Gabriel Volpe <volpegabriel@gmail.com>
2019-09-17 21:53:48 -0500

    The function pickOne will now pick a unique sticker

--- src/main/scala/io/github/gvolpe/CoolStickers.scala
+++ src/main/scala/io/github/gvolpe/CoolStickers.scala
@@ -1,9 +1,11 @@
 package io.github.gvolpe.bot
 
 import cats.effect._
+import cats.effect.implicits._
+import cats.effect.concurrent.Ref
 import cats.implicits._
-import canoe.models.outgoing.StickerContent
 import canoe.models.InputFile.Existing
+import canoe.models.outgoing.StickerContent
 import scala.util.Random
 
 trait CoolStickers[F[_]] {
@@ -12,21 +14,52 @@ trait CoolStickers[F[_]] {
 
 object CoolStickers {
   def make[F[_]: Sync]: F[CoolStickers[F]] =
-    Sync[F].delay(new scala.util.Random()).map(rnd => new LiveCoolStickers[F](rnd)).widen
+    Ref.of[F, Vector[String]](Vector.empty).flatMap { ref =>
+      Sync[F].delay(new scala.util.Random()).map { rnd =>
+        new LiveCoolStickers[F](rnd, ref)
+      }
+    }
 }
 
-private class LiveCoolStickers[F[_]: Sync](rnd: Random) extends CoolStickers[F] {
+private class LiveCoolStickers[F[_]: Sync](
+    rnd: Random,
+    ref: Ref[F, Vector[String]]
+) extends CoolStickers[F] {
 
   private val stickers = List(
     "CAADAgADDwADmL-ADa3Ih1FEY1UeFgQ", // alpaca
-    "CAADBQADtgMAAukKyAN_IGYzBDoeZxYE", // bj
     "CAADAgADxQYAApb6EgXIFcp2PSLK4RYE", // b&b
     "CAADBAADLQADmDVxAtmLKycYAVEYFgQ", // futurama
     "CAADAgAD_QADDbtAAAHdGnqdnefzlRYE", // pickle rick
     "CAADAgADUAMAAkcVaAnTqCldmOjPDRYE", // homer
+    "CAADAgADxxcAAkKvaQABT0psugtIvgwWBA", // yoda
+    "CAADAgADcAMAAs9fiwfBgbTnPr0n8xYE", // walter white
+    "CAADBAADVwADyt9oBSRp14BAcTAfFgQ", // shrug (c&h)
+    "CAADBAADdQEAAuJy2QABYH_5z-AqV2cWBA", // mr bean
+    "CAADAgADWgcAAlOx9wOIad-ZBUtESxYE", // adventure
+    "CAADBAADEwADmDVxAp3k1xTFyNcyFgQ", // bender
+    "CAADBQADbwMAAukKyAOvzr7ZArpddBYE", // toast (di caprio)
+    "CAADBQADgAMAAukKyAOXWG874z7K-BYE", // yup (meme)
+    "CAADBQADqgMAAukKyAOMMrddotAFYRYE", // travolta (confused)
+    "CAADAgADBgADwDZPE8fKovSybnB2FgQ", // hot cherry
     "CAADAgADHwADDbtAAAEE7ffQ20tl7hYE" // rick
-  ).map(id => StickerContent(Existing(id)))
+  )
 
-  def pickOne: F[StickerContent] =
-    Sync[F].delay(rnd.nextInt(stickers.size)).map(n => stickers(n))
+  def pickOne: F[StickerContent] = {
+    // When the vector size == stickers size we reset our state to start again
+    def maybeResetState: F[Unit] =
+      ref.get.map(_.size == stickers.size).ifM(ref.set(Vector.empty), ().pure[F])
+
+    def findUnique: F[String] =
+      for {
+        sticker <- Sync[F].delay(rnd.nextInt(stickers.size)).map(stickers.apply)
+        unique <- ref.get.map(_.find(_ == sticker)).flatMap {
+                   case Some(_) => findUnique
+                   case None    => sticker.pure[F].guarantee(ref.update(_.appended(sticker)))
+                 }
+      } yield unique
+
+    maybeResetState *> findUnique.map(id => StickerContent(Existing(id)))
+  }
+
 }
